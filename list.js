679142f2d07ab8e9f070d7312cbcaf88d6752f3a
misskayth11 <kei11073112@gmail.com>
2023-04-24 16:13:03 +0630

    list for fun
    好きなアイドルとかアニメとかリスト作ろう

---
+++ list.js
@@ -0,0 +1,21 @@
+alert('楽しもうね！');
+const form= document.querySelector('#listform');
+const btn=document.querySelector('#btn1');
+const input1=document.querySelector('#person');
+const list=document.querySelector('#reallist1');
+const btn2 =document.querySelector('#btn2');
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    let newLi1=document.createElement('li');
+    const personName=input1.value;
+    newLi1.innerText=personName;
+    list.append(newLi1);
+
+});
+list.addEventListener('click',function(e){
+    e.target.nodeName === 'LI' && e.target.remove();
+});
+
+btn2.addEventListener('click',function(){
+    console.log('clicked');
+});
